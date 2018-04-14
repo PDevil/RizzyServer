fe9f1080be24d1624894565ae0ff716152cbe6c6
PDevil <pawcio41@o2.pl>
2018-04-14 10:51:12 +0200

    Server properties setup

--- src/main/java/dev/lazyllamas/RizzyServer/RizzyServerApplication.java
+++ src/main/java/dev/lazyllamas/RizzyServer/RizzyServerApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RizzyServerApplication {
-
-	public static void main(String[] args) {
+public class RizzyServerApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(RizzyServerApplication.class, args);
 	}
 }
