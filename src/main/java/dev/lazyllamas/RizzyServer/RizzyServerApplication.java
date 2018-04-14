379f594b82fd638425b92218f14e2852425603d1
PDevil <pawcio41@o2.pl>
2018-04-14 10:35:27 +0200

    Spring Initializr files

---
+++ src/main/java/dev/lazyllamas/RizzyServer/RizzyServerApplication.java
@@ -0,0 +1,12 @@
+package dev.lazyllamas.RizzyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzyServerApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/lazyllamas/RizzyServer/RizzyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.lazyllamas.RizzyServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RizzyServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
