5715de1ba852c2d5dc111c8ce0f4bbdc5743d494
PDevil <pawcio41@o2.pl>
2018-04-15 14:57:09 +0200

    Final touches

--- src/main/java/dev/lazyllamas/RizzyServer/controllers/FriendshipController.java
+++ src/main/java/dev/lazyllamas/RizzyServer/controllers/FriendshipController.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/friends")
 public class FriendshipController
 {
+	// TODO
 }
--- src/main/java/dev/lazyllamas/RizzyServer/services/UserService.java
+++ src/main/java/dev/lazyllamas/RizzyServer/services/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import java.net.URI;
 import java.util.UUID;
 
 @Service
